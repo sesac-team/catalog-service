b12002fd801c052ef2bd399ae5492571aa6e87a9
gmdcjdakdcjd <ysh010223@naver.com>
2024-08-21 16:39:04 +0900

    kf-ad

--- src/test/java/com/polarbookshop/catalogservice/config/SecurityConfig.java
+++
@@ -1,7 +0,0 @@
-package com.polarbookshop.catalogservice.config;
-
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
-@EnableWebSecurity
-public class SecurityConfig {
-}
