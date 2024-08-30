1ae48cccfb95524bfe8c47588b5a60ba5a18e548
gmdcjdakdcjd <ysh010223@naver.com>
2024-08-30 13:53:21 +0900

    allow

--- src/main/java/com/polarbookshop/catalogservice/config/SecurityConfig.java
+++ src/main/java/com/polarbookshop/catalogservice/config/SecurityConfig.java
@@ -1,9 +1,14 @@
-//package com.polarbookshop.catalogservice.config;
-//
+package com.polarbookshop.catalogservice.config;
+
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Configuration;
 //import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.Customizer;
 //import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+//import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 //import org.springframework.security.web.SecurityFilterChain;
 //
 //@Configuration(proxyBeanMethods = false)
@@ -13,9 +18,25 @@
 //	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 //		return http
 //				.authorizeHttpRequests(authorize -> authorize
-//						.anyRequest().permitAll() // 모든 요청을 인증 없이 허용
+//						.requestMatchers(HttpMethod.GET, "/", "/books/**").permitAll()
+//						.anyRequest().hasRole("employee")
 //				)
-//				.csrf().disable() // CSRF 보호 비활성화
+//				.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+//				.sessionManagement(sessionManagement ->
+//						sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//				.csrf(AbstractHttpConfigurer::disable)
 //				.build();
 //	}
+//
+//	@Bean
+//	public JwtAuthenticationConverter jwtAuthenticationConverter() {
+//		var jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+//		jwtGrantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+//		jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("roles");
+//
+//		var jwtAuthenticationConverter = new JwtAuthenticationConverter();
+//		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+//		return jwtAuthenticationConverter;
+//	}
+//
 //}
